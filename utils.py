e3bd7cf739bc73bab58e9bb2510293c3788f5e6c
badiatul <164709920+badiatul@users.noreply.github.com>
2025-06-06 18:10:19 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import datetime
+
+def log_history(alg, mode, input_text, result):
+    if "history" not in st.session_state:
+        st.session_state["history"] = []
+    st.session_state["history"].append({
+        "algoritma": alg,
+        "mode": mode,
+        "input": input_text,
+        "hasil": result,
+        "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
+    st.session_state["last_result"] = result
