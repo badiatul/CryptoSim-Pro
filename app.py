3a1aa132d8459784c3510fbb8dc2b6993e98beff
badiatul <164709920+badiatul@users.noreply.github.com>
2025-06-15 13:04:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if choice == "Beranda":
     <p style='text-align: center; font-style: italic; color: #555;'>Aplikasi ini dibuat untuk memenuhi tugas UAS Pemrograman Kriptografi</p>
     <hr style='margin-top: 10px; margin-bottom: 20px;'>
     <ul style='line-height: 1.8; font-size: 16px;'>
-        <li>🔐 Gunakan berbagai algoritma kriptografi klasik dan modern</li>
+        <li>🔐 Gunakan beberapa algoritma kriptografi klasik dan modern</li>
         <li>📁 Unggah file, lihat hasil, download, dan dapatkan QR Code</li>
         <li>🚀 Silakan pilih algoritma di sidebar untuk memulai</li>
     </ul>
